861d9cecb55cfc134ea8a886610cc2d23dd9c00d
TheRealDelinquentRP <59104340+TheRealDelinquentRP@users.noreply.github.com>
2020-01-06 16:03:34 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,53 +2,6 @@ Config = {}
 
 --Blips
 Config.MainSprite = 486881925 -- Job Blip sprite
-Config.DeliverySprite = -695368421 -- Point Sprite
-Config.ShowBlips = true -- Show Point blips
-
---Messages
-Config.Deliver = "Press (E) to make a delivery" -- Start Gathering Message
-Config.TimerMsg = "Delivering" -- Finish message
-
---Timer and zone size
-Config.DeliveryTime = 10000 -- Time it takes to deliver (10 seconds)
-Config.ZoneSize = 2.0 -- Sizes of the zones
-
---Point specifics
-
---Valentine to Fort Wallace
-Config.MainJob1 = {x=-326.08,y=805.85,z=117.88} -- Job1 Position Valentine
-Config.Point1 = {x=367.17,y=1471.6,z=180.22} -- Point 1 location Fort Wallace
-Config.StartWorking1 = "Press (E) to take supplies to Fort Wallace." -- Start Working Message
-Config.Cash1 = 5 -- Cash payout
-Config.Exp1 = 1 -- EXP payout
-Config.Gold1 = 1 -- Gold payout
-
---Annesburg to Emerald Ranch
-Config.MainJob2 = {x=2939.25,y=1288.65,z=44.65} -- Job2 Position Annesburg
-Config.Point2 = {x=1420.15,y=383.93,z=90.33} -- Point 2 location Emerald Ranch
-Config.StartWorking2 = "Press (E) to take supplies to Emerald Ranch." -- Start Working Message
-Config.Cash2 = 8 -- Cash payout
-Config.Exp2 = 2 -- EXP payout
-Config.Gold2 = 0 -- Gold payout
-
---Blackwater to Strawberry
-Config.MainJob3 = {x=-778.33,y=-1323.44,z=43.88} -- Job3 Position Blackwater
-Config.Point3 = {x=-1792.18,y=-391.68,z=160.26} -- Point 3 location Strawberry
-Config.StartWorking3 = "Press (E) to take supplies to Strawberry." -- Start Working Message
-Config.Cash3 = 7 -- Cash payout
-Config.Exp3 = 2 -- EXP payout
-Config.Gold3 = 0 -- Gold payout
-
---MacFarland Ranch to Armadillo
-Config.MainJob4 = {x=-2324.89,y=-2406.17,z=63.85} -- Job4 Position MacFarland
-Config.Point4 = {x=-3686.81,y=-2627.33,z=-13.43} -- Point 4 location Armadillo
-Config.StartWorking4 = "Press (E) to take supplies to Armadillo." -- Start Working Message
-Config.Cash4 = 10 -- Cash payout
-Config.Exp4 = 3 -- EXP payout
-Config.Gold4 = 2 -- Gold payoutConfig = {}
-
---Blips
-Config.MainSprite = 486881925 -- Job Blip sprite
 Config.DeliverySprite = -570710357 -- Point Sprite
 Config.ShowBlips = true -- Show Point blips
 
