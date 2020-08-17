d4494c518858765df00caf61c897682ae3dfb6b9
TheRealDelinquentRP <59104340+TheRealDelinquentRP@users.noreply.github.com>
2020-08-17 17:01:35 -0400

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -26,7 +26,7 @@ Citizen.CreateThread(function()
 		local betweencoords3 = GetDistanceBetweenCoords(coords.x, coords.y, coords.z, Config.MainJob3.x, Config.MainJob3.y, Config.MainJob3.z, true)
 		local betweencoords4 = GetDistanceBetweenCoords(coords.x, coords.y, coords.z, Config.MainJob4.x, Config.MainJob4.y, Config.MainJob4.z, true)
 		if betweencoords <= 2.2 then
-		DrawTxt(Config.StartWorking1, 0.50, 0.90, 0.7, 0.7, true, 255, 255, 255, 255, true)
+		DrawText(Config.StartWorking1, 0.50, 0.90)
 			if IsControlJustPressed(2, Config.JobButton) then
 				p1 = N_0x554d9d53f696d002(1664425300, Config.Point1.x, Config.Point1.y, Config.Point1.z)
 					SetBlipSprite(p1, Config.DeliverySprite, 1)
@@ -35,7 +35,7 @@ Citizen.CreateThread(function()
 				DoScreenFadeOut(1500)
 				Wait(1500)
 				cart()
-				StartGpsMultiRoute(GetHashKey("COLOR_RED"), true, true)
+				StartGpsMultiRoute(GetHashKey("COLOR_YELLOW"), true, true)
         
 				-- Add the points
 				AddPointToGpsMultiRoute(Config.Point1.x, Config.Point1.y, Config.Point1.z)
@@ -44,11 +44,11 @@ Citizen.CreateThread(function()
 				SetGpsMultiRouteRender(true)
 				Wait(500)
 				DoScreenFadeIn(1500)
-				DrawTxt("Delivery started", 0.50, 0.90, 0.7, 0.7, true, 255, 255, 255, 255, true)
+				Wait(2500)
 			end
 
 		elseif betweencoords2 <= 2.2 then
-		DrawTxt(Config.StartWorking2, 0.50, 0.90, 0.7, 0.7, true, 255, 255, 255, 255, true)
+		DrawText(Config.StartWorking2, 0.50, 0.90)
 			if IsControlJustPressed(2, Config.JobButton) then
 				p2 = N_0x554d9d53f696d002(1664425300, Config.Point2.x, Config.Point2.y, Config.Point2.z)
 					SetBlipSprite(p2, Config.DeliverySprite, 1)
@@ -57,7 +57,7 @@ Citizen.CreateThread(function()
 				DoScreenFadeOut(1500)
 				Wait(1500)
 				cart2()
-				StartGpsMultiRoute(GetHashKey("COLOR_RED"), true, true)
+				StartGpsMultiRoute(GetHashKey("COLOR_YELLOW"), true, true)
         
 				-- Add the points
 				AddPointToGpsMultiRoute(Config.Point2.x, Config.Point2.y, Config.Point2.z)
@@ -66,11 +66,11 @@ Citizen.CreateThread(function()
 				SetGpsMultiRouteRender(true)
 				Wait(500)
 				DoScreenFadeIn(1500)
-				DrawTxt("Delivery started", 0.50, 0.90, 0.7, 0.7, true, 255, 255, 255, 255, true)
+				Wait(2500)
 			end
 
 		elseif betweencoords3 <= 2.2 then
-		DrawTxt(Config.StartWorking3, 0.50, 0.90, 0.7, 0.7, true, 255, 255, 255, 255, true)
+		DrawText(Config.StartWorking3, 0.50, 0.90)
 			if IsControlJustPressed(2, Config.JobButton) then
 				p3 = N_0x554d9d53f696d002(1664425300, Config.Point3.x, Config.Point3.y, Config.Point3.z)
 					SetBlipSprite(p3, Config.DeliverySprite, 1)
@@ -79,7 +79,7 @@ Citizen.CreateThread(function()
 				DoScreenFadeOut(1500)
 				Wait(1500)
 				cart3()
-				StartGpsMultiRoute(GetHashKey("COLOR_RED"), true, true)
+				StartGpsMultiRoute(GetHashKey("COLOR_YELLOW"), true, true)
         
 				-- Add the points
 				AddPointToGpsMultiRoute(Config.Point3.x, Config.Point3.y, Config.Point3.z)
@@ -88,11 +88,11 @@ Citizen.CreateThread(function()
 				SetGpsMultiRouteRender(true)
 				Wait(500)
 				DoScreenFadeIn(1500)
-				DrawTxt("Delivery started", 0.50, 0.90, 0.7, 0.7, true, 255, 255, 255, 255, true)
+				Wait(2500)
 			end
 
 		elseif betweencoords4 <= 2.2 then
-		DrawTxt(Config.StartWorking4, 0.50, 0.90, 0.7, 0.7, true, 255, 255, 255, 255, true)
+		DrawText(Config.StartWorking4, 0.50, 0.90)
 			if IsControlJustPressed(2, Config.JobButton) then
 				p4 = N_0x554d9d53f696d002(1664425300, Config.Point4.x, Config.Point4.y, Config.Point4.z)
 					SetBlipSprite(p4, Config.DeliverySprite, 1)
@@ -101,7 +101,7 @@ Citizen.CreateThread(function()
 				DoScreenFadeOut(1500)
 				Wait(1500)
 				cart4()
-				StartGpsMultiRoute(GetHashKey("COLOR_RED"), true, true)
+				StartGpsMultiRoute(GetHashKey("COLOR_YELLOW"), true, true)
         
 				-- Add the points
 				AddPointToGpsMultiRoute(Config.Point4.x, Config.Point4.y, Config.Point4.z)
@@ -110,19 +110,20 @@ Citizen.CreateThread(function()
 				SetGpsMultiRouteRender(true)
 				Wait(500)
 				DoScreenFadeIn(1500)
-				DrawTxt("Delivery started", 0.50, 0.90, 0.7, 0.7, true, 255, 255, 255, 255, true)
+				Wait(2500)
 			end
 		else end
 	end
 end)
 
-function DrawTxt(_message, x, y, w, h, enableShadow, col1, col2, col3, a, centre)
-    local str = Citizen.InvokeNative(0xFA925AC00EB830B9, 10, "LITERAL_STRING", _message, Citizen.ResultAsLong())
-       SetTextScale(w, h)
-       SetTextCentre(centre)
-       Citizen.InvokeNative(0xFA233F8FE190514C, str)
-       Citizen.InvokeNative(0xE9990552DEC71600)
-   end
+function DrawText(text,x,y)
+    SetTextScale(0.50,0.50)
+    SetTextColor(255,255,255,255)--r,g,b,a
+    SetTextCentre(true)--true,false
+    SetTextDropshadow(1,0,0,0,200)--distance,r,g,b,a
+    SetTextFontForCurrentCommand(1)
+    DisplayText(CreateVarString(10, "LITERAL_STRING", text), x, y)
+end
 
 Citizen.CreateThread(function()
 while true do
@@ -134,9 +135,9 @@ while true do
 	local betweencoords3 = GetDistanceBetweenCoords(coords.x, coords.y, coords.z, Config.Point3.x, Config.Point3.y, Config.Point3.z, true)
 	local betweencoords4 = GetDistanceBetweenCoords(coords.x, coords.y, coords.z, Config.Point4.x, Config.Point4.y, Config.Point4.z, true)
 	if betweencoords <= Config.ZoneSize and isDelivering then
-	DrawTxt(Config.Deliver, 0.50, 0.90, 0.7, 0.7, true, 255, 255, 255, 255, true)
+	DrawText(Config.Deliver, 0.50, 0.90)
 		if IsControlJustPressed(2, Config.DeliveryButton) then
-			TriggerEvent("redemrp_notification:start", "Delivering" , 1, "warning")
+			
 			TriggerServerEvent('redemrp_Deliveryjob:payout', Config.Cash1, Config.Gold1, Config.EXP1)
 				isDelivering = false
 				RemoveBlip(p1)
@@ -146,12 +147,12 @@ while true do
 				SetGpsMultiRouteRender(false)
 				Wait(500)
 				DoScreenFadeIn(1500)
-				DrawTxt("Delivery complete", 0.50, 0.90, 0.7, 0.7, true, 255, 255, 255, 255, true)
+				TriggerEvent("redem_roleplay:NotifyLeft", 'Delivery Complete', 'You have been paid ~e~$~t6~'..Config.Cash1..'~q~ and ~o~'..Config.Gold1..'~q~ gold', 'generic_textures', 'tick', 8000)
 			end
 	elseif betweencoords2 <= Config.ZoneSize and isDelivering then
-	DrawTxt(Config.Deliver, 0.50, 0.90, 0.7, 0.7, true, 255, 255, 255, 255, true)
+	DrawText(Config.Deliver, 0.50, 0.90)
 		if IsControlJustPressed(2, Config.DeliveryButton) then
-			TriggerEvent("redemrp_notification:start", "Delivering" , 1, "warning")
+			
 			TriggerServerEvent('redemrp_Deliveryjob:payout', Config.Cash2, Config.Gold2, Config.EXP2)
 				isDelivering = false
 				RemoveBlip(p2)
@@ -161,12 +162,12 @@ while true do
 				SetGpsMultiRouteRender(false)
 				Wait(500)
 				DoScreenFadeIn(1500)
-				DrawTxt("Delivery complete", 0.50, 0.90, 0.7, 0.7, true, 255, 255, 255, 255, true)
+				TriggerEvent("redem_roleplay:NotifyLeft", 'Delivery Complete', 'You have been paid ~e~$~t6~'..Config.Cash2..'~q~ and ~o~'..Config.Gold2..'~q~ gold', 'generic_textures', 'tick', 8000)
 			end
 	elseif betweencoords3 <= Config.ZoneSize and isDelivering then
-	DrawTxt(Config.Deliver, 0.50, 0.90, 0.7, 0.7, true, 255, 255, 255, 255, true)
+	DrawText(Config.Deliver, 0.50, 0.90)
 		if IsControlJustPressed(2, Config.DeliveryButton) then
-			TriggerEvent("redemrp_notification:start", "Delivering" , 1, "warning")
+			
 			TriggerServerEvent('redemrp_Deliveryjob:payout', Config.Cash3, Config.Gold3, Config.EXP3)
 				isDelivering = false
 				RemoveBlip(p3)
@@ -176,13 +177,13 @@ while true do
 				SetGpsMultiRouteRender(false)
 				Wait(500)
 				DoScreenFadeIn(1500)
-				DrawTxt("Delivery complete", 0.50, 0.90, 0.7, 0.7, true, 255, 255, 255, 255, true)
+				TriggerEvent("redem_roleplay:NotifyLeft", 'Delivery Complete', 'You have been paid ~e~$~t6~'..Config.Cash3..'~q~ and ~o~'..Config.Gold3..'~q~ gold', 'generic_textures', 'tick', 8000)
 			end
 			
 	elseif betweencoords4 <= Config.ZoneSize and isDelivering then
-	DrawTxt(Config.Deliver, 0.50, 0.90, 0.7, 0.7, true, 255, 255, 255, 255, true)
+	DrawText(Config.Deliver, 0.50, 0.90)
 		if IsControlJustPressed(2, Config.DeliveryButton) then
-			TriggerEvent("redemrp_notification:start", "Delivering" , 1, "warning")
+			
 			TriggerServerEvent('redemrp_Deliveryjob:payout', Config.Cash4, Config.Gold4, Config.EXP4)
 				isDelivering = false
 				RemoveBlip(p4)
@@ -192,7 +193,7 @@ while true do
 				SetGpsMultiRouteRender(false)
 				Wait(500)
 				DoScreenFadeIn(1500)
-				DrawTxt("Delivery complete", 0.50, 0.90, 0.7, 0.7, true, 255, 255, 255, 255, true)
+				TriggerEvent("redem_roleplay:NotifyLeft", 'Delivery Complete', 'You have been paid ~e~$~t6~'..Config.Cash4..'~q~ and ~o~'..Config.Gold4..'~q~ gold', 'generic_textures', 'tick', 8000)
 			end
 	else end
 	end
@@ -281,3 +282,23 @@ function cart4()
 	Citizen.InvokeNative(0xD80FAF919A2E56EA, car4, 2031331786)--add cargo
     SetModelAsNoLongerNeeded(carHash)
 end
+
+--prevents the native bugging if you restart script
+AddEventHandler('onResourceStop', function(resourceName)
+	if (GetCurrentResourceName() ~= resourceName) then
+	  return
+	end
+	RemoveBlip(p1)
+	RemoveBlip(p2)
+	RemoveBlip(p3)
+	RemoveBlip(p4)
+	p1 = nil
+	p2 = nil
+	p3 = nil
+    p4 = nil
+	SetGpsMultiRouteRender(false)
+	DeleteVehicle(car)
+	DeleteVehicle(car2)
+	DeleteVehicle(car3)
+	DeleteVehicle(car4)
+end)
