2bfee2243c46b72e44831d9f6b4be0e38036cc95
TheRealDelinquentRP <59104340+TheRealDelinquentRP@users.noreply.github.com>
2020-08-17 15:19:20 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -30,7 +30,7 @@ Config.Gold1 = 0 -- Gold payout
 Config.MainJob2 = {x=-179.94,y=626.87,z=114.09} -- Job2 Position Valentine
 Config.Point2 = {x=-1311.12,y=385.71,z=95.70} -- Point 2 location Wallace Station
 Config.StartWorking2 = "Press ~INPUT_JUMP~ to take supplies to Wallace Station." -- Start Working Message
-Config.Cart2 = {x=-185.82,y=649.83,z=112.72,h=61.97} -- Cart spawn point
+Config.Cart2 = {x=-185.82,y=649.83,z=112.53,h=61.97} -- Cart spawn point
 Config.Cash2 = 7 -- Cash payout
 Config.EXP2 = 2 -- EXP payout
 Config.Gold2 = 0 -- Gold payout
@@ -38,7 +38,7 @@ Config.Gold2 = 0 -- Gold payout
 --Blackwater to MacFarland Ranch
 Config.MainJob3 = {x=-732.8,y=-1221.9,z=44.73} -- Job3 Position Blackwater
 Config.Point3 = {x=-2342.75,y=-2396.24,z=62.37} -- Point 3 location MacFarlandRanch
-Config.Cart3 = {x=-760.18,y=-1239.44,z=42.7,h=246.13} -- Cart spawn point
+Config.Cart3 = {x=-760.18,y=-1239.44,z=42.63,h=246.13} -- Cart spawn point
 Config.StartWorking3 = "Press ~INPUT_JUMP~ to take supplies to MacFarland Ranch." -- Start Working Message
 Config.Cash3 = 6 -- Cash payout
 Config.EXP3 = 2 -- EXP payout
@@ -47,7 +47,7 @@ Config.Gold3 = 0 -- Gold payout
 --Braithwaite farm to Saint Denis
 Config.MainJob4 = {x=1087.4,y=-1726.79,z=50.08} -- Job4 Position MacFarland
 Config.Point4 = {x=2809.39,y=-1336.79,z=46.25} -- Point 4 location Armadillo
-Config.Cart4 = {x=1071.71,y=-1708.79,z=48.12,h=0.0} -- Cart spawn point
+Config.Cart4 = {x=1071.71,y=-1708.79,z=47.72,h=0.0} -- Cart spawn point
 Config.StartWorking4 = "Press ~INPUT_JUMP~ to take produce to Saint Denis." -- Start Working Message
 Config.Cash4 = 5 -- Cash payout
 Config.EXP4 = 2 -- EXP payout
